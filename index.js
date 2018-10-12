66fba3607b0dbaaecfcb870c9ef159c7f4d75045
Adi Faidz <adifaidzabdullah@gmail.com>
2018-10-13 01:39:19 +0800

    Added heroku keep awake function

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const {Client} = require('discord.js');
 const client = new Client();
 const PREFIX = process.env.BOT_PREFIX;
 
+var http = require("http");
+setInterval(function () {
+    http.get("http://fpl-discord-bot.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
