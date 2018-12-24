2d58136ea9e6de64f86792693dc5b5df632eb980
Adi Faidz <adifaidzabdullah@gmail.com>
2018-12-24 18:08:55 +0800

    Added simple bugsnag implementation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
 var http = require("http")
 setInterval(function () {
     http.get("http://cinema-discord-bot.herokuapp.com/")
 }, 300000) // every 5 minutes (300000)
 
 require('dotenv').config()
+var bugsnag = require('@bugsnag/js')
+var bugsnagClient = bugsnag(process.env.BUGSNAG_API_KEY)
+
 const path = require('path')
 const { CommandoClient } = require('discord.js-commando')
 const client = new CommandoClient({
